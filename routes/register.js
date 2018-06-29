062433937c7f29372298c2b879a3bb6170828624
cvdat2097 <33714178+cvdat2097@users.noreply.github.com>
2018-06-29 11:10:12 +0700

    sdf

--- routes/register.js
+++ routes/register.js
@@ -9,6 +9,37 @@ function validateEmail(email) {
   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
 }
+router.get('/registerUser', function (req, res, next) {
+
+  var name = req.param('name');
+  var username = req.param('username');
+  var phone = req.param('phone');
+  var address = req.param('address');
+  var email = req.param('email');
+  var password = req.param('password');
+  var type = req.param('type');
+  User.findOne(username, function (err, userFound) {
+    if (userFound == null) {
+    } else {
+      res.send('existed');
+    }
+  })
+  business.RegisterNewUser(type, username, password, name, email, phone, address, function (success) {
+    res.send(success.toString());
+  });
+});
+
+router.get('/checkphonenumber', function (req, res, next) {
+  var phone = req.param('phone');
+
+  if (phone.length >= 9) {
+    res.send('true');
+  } else {
+    res.send('false');
+  }
+
+})
+
 router.get('/', function (req, res, next) {
   renderer.RegisterPage.RenderRegisterPage(req, res, next);
 });
@@ -23,25 +54,25 @@ router.get('/registerUser', function (req, res, next) {
 
   //check mail
   if (validateEmail(email)) {
-    } else {
-      res.send('notMail');
-      return;
-    }
+  } else {
+    res.send('notMail');
+    return;
+  }
   if (isPhoneNumber(phone)) {
   } else {
     res.send('notPhone');
     return;
   }
-  User.findOne({username: username},function (err, userFound) {
-	if (userFound == null) {
-    business.RegisterNewUser(type, username, password, name, email, phone, address,function (success) {
-    res.send(success.toString());
-    return;
-  });
-	} else {
-		res.send('existed');
-    return;
-	}
+  User.findOne({ username: username }, function (err, userFound) {
+    if (userFound == null) {
+      business.RegisterNewUser(type, username, password, name, email, phone, address, function (success) {
+        res.send(success.toString());
+        return;
+      });
+    } else {
+      res.send('existed');
+      return;
+    }
   })
 });
 module.exports = router;
